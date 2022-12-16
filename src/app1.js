695b2aa996521b6455a2bb23ba9585e3b89f105f
WangDanmeng <WangDanmeng98@163.com>
2022-12-16 11:17:08 +0800

    引入Vue

--- src/app1.js
+++ src/app1.js
@@ -2,68 +2,95 @@ import $ from 'jquery'
 import './app1.css'
 import Model from "./base/Model";
 import View from "./base/View";
-import EventBus from "./base/EventBus";
+import Vue from 'vue'
 
-// const eventBus = new EventBus()
-
-// ----------------- 数据相关 m -----------------
-const m = new Model({
-    data: {
-        // 初始化数据
-        n: parseFloat(localStorage.getItem('res'))
-    },
-    update: function (data) {
-        Object.assign(m.data, data)
-        m.trigger('m:update') // 触发事件
-        localStorage.setItem('res', m.data.n)
-    }
-})
-
-// ----------------- 其它 c -----------------
+// const m = new Model({
+//     data: {
+//         n: parseFloat(localStorage.getItem('res'))
+//     },
+//     update: function (data) {
+//         Object.assign(m.data, data)
+//         m.trigger('m:update') // 触发事件
+//         localStorage.setItem('res', m.data.n)
+//     }
+// })
 
 
 const init = (el) => {
-    new View({
-        // 初始化, 形参为容器
+    new Vue({
         el: el,
-        data: m.data,
-        html: `
-            <div>  
-                <div class="result">计算结果: <span id="resultNum">{{n}}</span></div>
-                <button class="btn add">加1</button>
-                <button class="btn sub">减1</button>
-                <button class="btn mul">乘2</button>
-                <button class="btn div">除2</button>
-            </div>
-         `,
-        render(data) {
-            const n = data.n
-            if (this.el.children.length !== 0) {
-                // 清空当前html,重新渲染
-                this.el.empty()
+        data: {n: parseFloat(localStorage.getItem('res'))},
+        template: `
+          <section class="app app1">
+          <div class="result">计算结果: <span id="resultNum">{{ n }}</span></div>
+          <button class="btn add" @click="add">加1</button>
+          <button class="btn sub" @click="sub">减1</button>
+          <button class="btn mul" @click="mul">乘2</button>
+          <button class="btn div" @click="div">除2</button>
+          </section>
+        `,
+        methods: {
+            add() {
+                this.n += 1
+            },
+            sub() {
+                this.n -= 1
+            },
+            mul() {
+                this.n *= 2
+            },
+            div() {
+                this.n /= 2
             }
-            $(this.html.replace('{{n}}', n)).appendTo(this.el)
         },
-        events: {
-            'click .add': 'add',
-            'click .sub': 'sub',
-            'click .mul': 'mul',
-            'click .div': 'div'
-        },
-        add() {
-            m.update({n: m.data.n + 1})
-        },
-        sub() {
-            m.update({n: m.data.n - 1})
-        },
-        mul() {
-            m.update({n: m.data.n * 2})
-        },
-        div() {
-            m.update({n: m.data.n / 2})
+        watch: {
+            n() {
+                localStorage.setItem('res', this.n)
+            }
         }
-
     })
+    // return
+    // new View({
+    //     // 初始化, 形参为容器
+    //     el: el,
+    //     data: m.data,
+    //     html: `
+    //         <div>
+    //             <div class="result">计算结果: <span id="resultNum">{{n}}</span></div>
+    //             <button class="btn add">加1</button>
+    //             <button class="btn sub">减1</button>
+    //             <button class="btn mul">乘2</button>
+    //             <button class="btn div">除2</button>
+    //         </div>
+    //      `,
+    //     render(data) {
+    //         const n = data.n
+    //         if (this.el.children.length !== 0) {
+    //             // 清空当前html,重新渲染
+    //             this.el.empty()
+    //         }
+    //         $(this.html.replace('{{n}}', n)).appendTo(this.el)
+    //     },
+    //     events: {
+    //         'click .add': 'add',
+    //         'click .sub': 'sub',
+    //         'click .mul': 'mul',
+    //         'click .div': 'div'
+    //     },
+    //     add() {
+    //         m.update({n: m.data.n + 1})
+    //     },
+    //     sub() {
+    //         m.update({n: m.data.n - 1})
+    //     },
+    //     mul() {
+    //         m.update({n: m.data.n * 2})
+    //     },
+    //     div() {
+    //         m.update({n: m.data.n / 2})
+    //     }
+    //
+    // })
 }
 export default init
 
--- src/app2.js
+++ src/app2.js
@@ -2,54 +2,72 @@ import './app2.css'
 import $ from 'jquery'
 import View from "./base/View";
 import Model from "./base/Model";
+import Vue from 'vue'
 
-const localKey = 'app2-index'
-const m = new Model({
-    data: {
-        index: parseInt(localStorage.getItem(localKey)) || 0
-    },
-    update(data) {
-        Object.assign(m.data, data)
-        m.trigger('m:update') // 触发事件
-        localStorage.setItem(localKey, m.data.index)
-    }
-})
+// const localKey = 'app2-index'
+// const m = new Model({
+//     data: {
+//         index: parseInt(localStorage.getItem(localKey)) || 0
+//     },
+//     update(data) {
+//         Object.assign(m.data, data)
+//         m.trigger('m:update') // 触发事件
+//         localStorage.setItem(localKey, m.data.index)
+//     }
+// })
 
 
 const init = (el) => {
-    new View({
+    new Vue({
         el: el,
-        html: (index) => {
-            return `
-        <div>
+        data: {index: 0},
+        template: `
+          <section class="app app2">
           <ol class="tab-bar">
-            <li class="${index === 0 ? 'active' : ''}" data-index="0">tab1</li>
-            <li class="${index === 1 ? 'active' : ''}" data-index="1">tab2</li>
+            <li :class="index === 0 ? 'active' : ''" @click="index=0">tab1</li>
+            <li :class="index === 1 ? 'active' : ''" @click="index=1">tab2</li>
           </ol>
           <ol class="tab-content">
-            <li class="${index === 0 ? 'active' : ''}">content1</li>
-            <li class="${index === 1 ? 'active' : ''}">content2</li>
+            <li :class="index === 0 ? 'active' : ''">content1</li>
+            <li :class="index === 1 ? 'active' : ''">content2</li>
           </ol>
-        </div>
+          </section>
         `
-        },
-        data: m.data,
-        render(data) {
-            const index = data.index
-            if (this.el.children.length !== 0) {
-                // 清空当前html,重新渲染
-                this.el.empty()
-            }
-            $(this.html(index)).appendTo(this.el)
-        },
-        events: {
-            'click .tab-bar>li': 'x',
-        },
-        x(e) {
-            const index = parseInt(e.currentTarget.dataset.index)
-            m.update({index: index})
-        },
     })
+    // return
+    // new View({
+    //     el: el,
+    //     html: (index) => {
+    //         return `
+    //     <div>
+    //       <ol class="tab-bar">
+    //         <li class="${index === 0 ? 'active' : ''}" data-index="0">tab1</li>
+    //         <li class="${index === 1 ? 'active' : ''}" data-index="1">tab2</li>
+    //       </ol>
+    //       <ol class="tab-content">
+    //         <li class="${index === 0 ? 'active' : ''}">content1</li>
+    //         <li class="${index === 1 ? 'active' : ''}">content2</li>
+    //       </ol>
+    //     </div>
+    //     `
+    //     },
+    //     data: m.data,
+    //     render(data) {
+    //         const index = data.index
+    //         if (this.el.children.length !== 0) {
+    //             // 清空当前html,重新渲染
+    //             this.el.empty()
+    //         }
+    //         $(this.html(index)).appendTo(this.el)
+    //     },
+    //     events: {
+    //         'click .tab-bar>li': 'x',
+    //     },
+    //     x(e) {
+    //         const index = parseInt(e.currentTarget.dataset.index)
+    //         m.update({index: index})
+    //     },
+    // })
 }
 
 
